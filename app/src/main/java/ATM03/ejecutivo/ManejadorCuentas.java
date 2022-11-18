f0753b3cbb4ae0c6468c3395929e3b7778d02079
Luis Armenta <luigiangelxd@gmail.com>
2022-11-17 21:07:23 -0600

    feat: interfaz para ejecutivos creada

---
+++ app/src/main/java/ATM03/ejecutivo/ManejadorCuentas.java
@@ -0,0 +1,25 @@
+package ATM03.ejecutivo;
+
+import ATM03.database.HashMapBinFileCuentasRepository;
+import ATM03.database.ICuentasRepository;
+import ATM03.models.Cuenta;
+
+public class ManejadorCuentas {
+  ICuentasRepository cuentasRepository;
+
+  public ManejadorCuentas() {
+    this.cuentasRepository = HashMapBinFileCuentasRepository.obtenerInstancia("archivo.bin");
+  }
+
+  public void crearCuenta(Cuenta cuenta) {
+    this.cuentasRepository.guardarNuevaCuenta(cuenta);
+  }
+
+  public void editarCuenta(Cuenta cuenta) {
+    this.cuentasRepository.actualizarCuenta(cuenta);
+  }
+
+  public void eliminarCuenta(int numeroCuenta) {
+    this.cuentasRepository.eleminarCuenta(numeroCuenta);
+  }
+}
